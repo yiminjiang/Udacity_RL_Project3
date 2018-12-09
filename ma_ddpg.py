0e3334a5a0ba76971476423e0a8d28a01b73d623
yiminjiang <43476087+yiminjiang@users.noreply.github.com>
2018-12-09 11:12:33 -0800

    Update ma_ddpg.py

--- ma_ddpg.py
+++ ma_ddpg.py
@@ -9,10 +9,10 @@ BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-4         # learning rate of the actor 
-LR_CRITIC = 3e-4        # learning rate of the critic
+LR_ACTOR = 1.2e-4         # learning rate of the actor 
+LR_CRITIC = 3.2e-4        # learning rate of the critic
 WEIGHT_DECAY = 0.0001        # L2 weight decay
-UPDATE_EVERY = 12
+UPDATE_EVERY = 10
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class MADDPG:
